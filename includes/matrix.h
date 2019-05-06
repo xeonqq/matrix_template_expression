56140e3da594913aaa16fdad554b030ecc1721e5
Qian <xeonqq@gmail.com>
2019-05-06 21:30:42 +0200

    Make Matrix two dimensional

--- includes/matrix.h
+++ includes/matrix.h
@@ -18,29 +18,32 @@ void logger(T&& s)
 template <typename T1, typename T2>
 class Sum;
 
-template <typename Value, size_t dim_w>
+template <typename Value, size_t dim_rows, size_t dim_cols>
 class Matrix
 {
-	using Array=std::array<Value, dim_w>;
-	using Self=Matrix<Value, dim_w>;
+	using Array=std::array<Value, dim_rows*dim_cols>;
+	using Self=Matrix<Value, dim_rows, dim_cols>;
 	public:
-		Matrix(Array array):array{std::move(array)} {}
+	Matrix(Array array):array{std::move(array)} {}
 
-		template <typename Exp1, typename Exp2>
+	template <typename Exp1, typename Exp2>
 		Matrix(Sum<Exp1, Exp2> sum) //copy constructor
 		{
 			logger("Copy Ctr in Matrix");
-			for (size_t i=0;i<array.size();++i)
+			for (size_t row=0;row<dim_rows;++row)
 			{
-				array[i] = sum(i);
+				for (size_t col=0;col<dim_cols;++col)
+				{
+					array[row * dim_cols + col] = sum(row, col);
+				}
 			}
 		}
 
-		const Array& row() const {return array;}
+	const Array& row() const {return array;}
 
-		auto operator() (size_t i) const
+	auto operator() (size_t row, size_t col) const
 		{
-			return array[i];
+			return array[row*dim_cols + col];
 		}
 
 	private:
@@ -55,9 +58,9 @@ class Sum
 	public:
 		Sum(const T1& exp1, const T2& exp2):exp1{exp1}, exp2{exp2} {}
 
-		auto operator() (size_t i) const
+		auto operator() (size_t row, size_t col) const
 		{
-			return exp1(i) + exp2(i);
+			return exp1(row, col) + exp2(row, col);
 		}
 	private:
 		const T1& exp1;
@@ -75,8 +78,8 @@ operator + (const Exp1& exp1, const Exp2& exp2)
 
 
 
-template <typename T, size_t dim_w>
-std::ostream& operator<<(std::ostream& os, const Matrix<T, dim_w>& mat)
+template <typename T, size_t dim_rows, size_t dim_cols>
+std::ostream& operator<<(std::ostream& os, const Matrix<T, dim_rows, dim_cols>& mat)
 {
 	std::for_each(mat.row().begin(), mat.row().end(), [&os](const auto& v){os<<v<<",";});
 	return os;
--- proxy.cpp
+++ proxy.cpp
@@ -3,27 +3,29 @@
 #include <string>
 #include <matrix.h>
 
-#define SIZE 2000
+#define ROWS 20
+#define COLS 20
+#define SIZE ROWS*COLS
 
-using Matrix2f = Matrix<double, SIZE>;
+using Matrix2f = Matrix<double, ROWS, COLS>;
 
 using namespace std;
 int main()
 {
 	std::array<double, SIZE> aa;
-	aa.fill(20.12);
+	aa.fill(1);
 
 	std::array<double, SIZE> bb;
-	bb.fill(31.2);
+	bb.fill(2);
 
 	std::array<double, SIZE> cc;
-	cc.fill(48.8);
+	cc.fill(3);
 
 	Matrix2f mat_a{std::move(aa)};
 	Matrix2f mat_b{std::move(bb)};
 	Matrix2f mat_c{std::move(cc)};
 
-	const Matrix2f sum = mat_a + mat_a + mat_a + mat_b + mat_c;
+	const Matrix2f sum = mat_a + mat_b + mat_c;
 	logger(sum);
 }
 
